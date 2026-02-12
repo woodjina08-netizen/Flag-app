1dfa5054c7029796206ea9198e8758919a559125
woodjina08-netizen <woodjina08@gmail.com>
2026-02-12 14:16:12 -0500

    Add country data

---
+++ data.js
@@ -0,0 +1,72 @@
+const countries = [
+  {
+    country: "United States",
+    capital: "Washington, D.C.",
+    flag: "🇺🇸",
+    language: "English",
+    continent: "North America"
+  },
+  {
+    country: "Canada",
+    capital: "Ottawa",
+    flag: "🇨🇦",
+    language: "English / French",
+    continent: "North America"
+  },
+  {
+    country: "Brazil",
+    capital: "Brasília",
+    flag: "🇧🇷",
+    language: "Portuguese",
+    continent: "South America"
+  },
+  {
+    country: "France",
+    capital: "Paris",
+    flag: "🇫🇷",
+    language: "French",
+    continent: "Europe"
+  },
+  {
+    country: "Germany",
+    capital: "Berlin",
+    flag: "🇩🇪",
+    language: "German",
+    continent: "Europe"
+  },
+  {
+    country: "Nigeria",
+    capital: "Abuja",
+    flag: "🇳🇬",
+    language: "English",
+    continent: "Africa"
+  },
+  {
+    country: "Egypt",
+    capital: "Cairo",
+    flag: "🇪🇬",
+    language: "Arabic",
+    continent: "Africa"
+  },
+  {
+    country: "Japan",
+    capital: "Tokyo",
+    flag: "🇯🇵",
+    language: "Japanese",
+    continent: "Asia"
+  },
+  {
+    country: "China",
+    capital: "Beijing",
+    flag: "🇨🇳",
+    language: "Mandarin",
+    continent: "Asia"
+  },
+  {
+    country: "Australia",
+    capital: "Canberra",
+    flag: "🇦🇺",
+    language: "English",
+    continent: "Oceania"
+  }
+];
